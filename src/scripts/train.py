0cad7ae44fcf2fd1d541ea487d8aa201c06a3f52
tonyroumi <tonyroumi@gmail.com>
2025-04-17 13:36:39 -0700

    feature-9/completes hydra logging

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -2,11 +2,6 @@ import os
 
 from src.locomotion import get_env_class
 
-xla_flags = os.environ.get("XLA_FLAGS", "")
-xla_flags += " --xla_gpu_triton_gemm_any=True"
-os.environ["XLA_FLAGS"] = xla_flags
-os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
-
 import time
 import json
 import hydra
@@ -21,6 +16,7 @@ from absl import logging
 
 from datetime import datetime
 from hydra.core.config_store import ConfigStore
+
 from src.config import Config
 # from src.locomotion.default_humanoid_legs.config.ppo_config import PPOConfig
 
@@ -43,6 +39,12 @@ from src.robots.robot import Robot
 
 import wandb
 
+xla_flags = os.environ.get("XLA_FLAGS", "")
+xla_flags += " --xla_gpu_triton_gemm_any=True"
+os.environ["XLA_FLAGS"] = xla_flags
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+os.environ["MUJOCO_GL"] = "egl"
+
 # Ignore the info logs from brax
 logging.set_verbosity(logging.WARNING)
 
@@ -56,6 +58,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning, module="jax")
 warnings.filterwarnings("ignore", category=UserWarning, module="absl")
 
 def train(
+    robot,
     env,
     eval_env,
     train_cfg,
@@ -67,9 +70,8 @@ def train(
     This function sets up the training environment, initializes configurations, and manages the training process.
     """
     
-
     #Change this to the hydra logging directory eventually
-    logdir = epath.Path("logs").resolve() / run_name
+    logdir = epath.Path("logs").resolve()
     logdir.mkdir(parents=True, exist_ok=True)
     print(f"Logs are being stored to {logdir}")
 
@@ -89,11 +91,16 @@ def train(
 
     ckpt_path = logdir / "checkpoints"
     ckpt_path.mkdir(parents=True, exist_ok=True)
-    print(f"Checkpoint path: {ckpt_path}")
+    print(f"Checkpoint path: {ckpt_path}")  
 
     #Save environment configuration
-    with open(ckpt_path / "config.json", "w") as f:
+    with open(logdir / "train_config.json", "w") as f:
         json.dump(OmegaConf.to_container(train_cfg), f, indent=4)
+
+
+    #Save robot configuration
+    with open(logdir / "robot_config.json", "w") as f:
+        json.dump(robot.config, f, indent=4)
 
 
     def policy_params_fn(current_step: int, make_policy: Any, params: Any):
@@ -172,6 +179,8 @@ def train(
     print(f"best checkpoint step: {best_ckpt_step}")
     print(f"best episode reward: {best_episode_reward}")
 
+    #let's save a rollout here 
+
 
 @hydra.main(config_path="../config", config_name="config")
 def main(cfg):
@@ -201,6 +210,7 @@ def main(cfg):
     print(f"Experiment name: {experiment_name}")
 
     train(
+        robot=robot,
         env=env,
         eval_env=eval_env,
         train_cfg=train_cfg,
